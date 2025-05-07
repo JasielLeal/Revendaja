6fad387584ebe54ac0f616f52738e06c295a0efd
Jasiel <jasieloficial@hotmail.com>
2025-05-07 07:41:18 -0300

    fix: update contact email and add google services configuration

--- revendaja-frontend/app/(routes)/(Site)/components/mobileNavBar.tsx
+++ revendaja-frontend/app/(routes)/(Site)/components/mobileNavBar.tsx
@@ -657,7 +657,7 @@ export default function Home() {
                     <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z" />
                     <polyline points="22,6 12,13 2,6" />
                   </svg>
-                  <span className="text-gray-400">contato@revendaja.com.br</span>
+                  <span className="text-gray-400">contato@revendaja.com</span>
                 </li>
               </ul>
             </div>
