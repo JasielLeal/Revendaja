b8b2f2d43a1d50363c4a947454ca707273edfefa
Jasiel <jasieloficial@hotmail.com>
2025-05-13 09:45:44 -0300

    refactor: remove Sidebar component and clean up layout

--- revendaja-frontend/app/(routes)/(appRoutes)/layout.tsx
+++ revendaja-frontend/app/(routes)/(appRoutes)/layout.tsx
@@ -2,8 +2,6 @@
 
 import { ReactNode } from "react";
 import * as React from "react"
-import Sidebar from "@/app/components/sidebar";
-
 
 interface AppLayoutProps {
     children: ReactNode;
@@ -14,7 +12,7 @@ export default function AppLayout({ children }: AppLayoutProps) {
 
     return (
         <div className="grid grid-cols-12">
-            <Sidebar />
+           
             <div className="col-span-10 bg-[#09090b] pt-10 px-10">
                 {children}
             </div>
