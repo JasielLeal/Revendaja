1ee955b0bf662106a3809d7a793b812d7249a1b7
Jasiel <jasieloficial@hotmail.com>
2025-05-30 14:09:08 -0300

    feat: Add theme context and toggle functionality for light/dark mode

--- revendaja/src/pages/appPages/home/components/toggleTheme.tsx
+++ revendaja/src/pages/appPages/home/components/toggleTheme.tsx
@@ -1,16 +1,15 @@
 import { Text, TouchableOpacity } from 'react-native';
 import {  useColorScheme } from "nativewind";
 import Icon from 'react-native-vector-icons/Ionicons'
+import { useTheme } from '@/context/themeContext';
 export function ThemeToggle() {
-
-    const { colorScheme, toggleColorScheme} = useColorScheme()
-
-    console.log('Current color scheme:', colorScheme);
+    
+     const { theme, toggleTheme } = useTheme();
 
     return (
-        <TouchableOpacity onPress={toggleColorScheme} className="p-4 bg-secondary  rounded-full mt-4">
+        <TouchableOpacity onPress={toggleTheme} className="p-4 bg-secondary  rounded-full mt-4">
             <Text className="text-white text-center">
-                {colorScheme === "dark" ? <Icon name='moon'/> : <Icon name='sunny'/>}
+                {theme === "dark" ? <Icon name='moon'/> : <Icon name='sunny'/>}
             </Text>
         </TouchableOpacity>
     );
--- revendaja/src/pages/appPages/home/home.tsx
+++ revendaja/src/pages/appPages/home/home.tsx
@@ -1,6 +1,6 @@
 import AuthContext from "@/context/authContext";
 import { useContext, useEffect, useState } from "react";
-import { Platform, Text, View } from "react-native";
+import { ActivityIndicator, Platform, Text, View } from "react-native";
 import Icon from 'react-native-vector-icons/Ionicons'
 import { TouchableOpacity } from "react-native";
 import { ForEveryDayLife } from "./components/forEveryDayLife";
@@ -14,11 +14,21 @@ import { StackNavigationProp } from "@react-navigation/stack";
 import { RootStackParamList } from "@/types/navigation";
 import { Avatar } from "@/components/avatart";
 import { ThemeToggle } from "./components/toggleTheme";
-import { useColorScheme } from "nativewind";
+import { useTheme } from "@/context/themeContext";
 
 export function Home() {
 
     const { user } = useContext(AuthContext)
+
+    const { isLoading } = useTheme();
+
+    if (isLoading) {
+        return (
+            <View className="flex-1 items-center justify-center bg-white dark:bg-black">
+                <ActivityIndicator size="large" color="#999" />
+            </View>
+        );
+    }
 
     const [hiddenMoney, setHiddenMoney] = useState(false)
 
